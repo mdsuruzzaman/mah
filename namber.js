cf9fed171f98d909972c65c4c56cdcd62a4a607b
mdsuruzzaman <mdsuruzzaman20@gmail.com>
2023-10-05 11:14:43 +0600

    rana

---
+++ namber.js
@@ -0,0 +1,17 @@
+// var main=["Mondol","Rakib","habib","mamun"];
+//  main.shift();
+// console.log(main);
+
+// let answer1 = "It's alright";
+// let answer2 = "He is called 'Johnny'";
+// let answer3 = 'He is called "Johnny"'; 
+
+// document.getElementById().innerHTML =
+// answer1 + "<br>" + answer2 + "<br>" + answer3; 
+
+document.write("anik")
+
+const sa = ["anki","mahfuz","sadn"];
+
+document.write("<br>"+sa.pop())
+document.write("<br>"+sa)
